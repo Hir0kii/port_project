7ede9bd6fdb7c82a9eccccecf34f37ac4230e59d
Hiroki <huchida0622@gmail.com>
2020-05-21 11:30:12 +0900

    add projects

--- jobs/models.py
+++ jobs/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Job(models.Model):
     title = models.CharField(max_length=200)
     image = models.ImageField(upload_to='images/')
+    url = models.URLField()
     summary = models.CharField(max_length=200)
 
     def __str__(self):
