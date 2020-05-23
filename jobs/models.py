94174dbd595be3db4217c79fa6d4af7cdce61429
Hiroki <huchida0622@gmail.com>
2020-05-23 23:24:04 +0900

    works comment

--- jobs/models.py
+++ jobs/models.py
@@ -5,7 +5,7 @@ class Job(models.Model):
     title = models.CharField(max_length=200)
     image = models.ImageField(upload_to='images/')
     url = models.URLField()
-    summary = models.CharField(max_length=200)
+    summary = models.TextField()
 
     def __str__(self):
         return self.title
